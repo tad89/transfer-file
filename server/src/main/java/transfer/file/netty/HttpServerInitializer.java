da5b1ef40e211e77e63a936823a54f335cd1e592
choiws <shotgun89@naver.com>
2023-04-17 17:01:14 +0900

    첫 커밋

---
+++ server/src/main/java/transfer/file/netty/HttpServerInitializer.java
@@ -0,0 +1,30 @@
+package transfer.file.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+    private final SslContext sslCtx;
+
+    public HttpServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpRequestDecoder());
+        // Uncomment the following line if you don't want to handle HttpChunks.
+        p.addLast(new HttpObjectAggregator(15 * 1024 * 1024));
+        p.addLast(new HttpResponseEncoder());
+        p.addLast(new HttpServerHandler());
+    }
+}
